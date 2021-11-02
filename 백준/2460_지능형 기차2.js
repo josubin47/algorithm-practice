f1da4a95a1b86c61cf17358640880ab782c7228d
josubin47 <3_3_jimca@naver.com>
2021-11-02 13:21:00 +0900

    불필요한 코드 제거

--- 백준/2460_지능형 기차2.js
+++ 백준/2460_지능형 기차2.js
@@ -16,24 +16,3 @@ for (let i = 1; i <= 8; i++) {
 }
 
 console.log(max);
-
-var a = 0;
-console.log(
-  require("fs")
-    .readFileSync("/dev/stdin")
-    .toString()
-    .trim()
-    .split("\n")
-    .map(function (e) {
-      return e.split(" ").map(function (e) {
-        return parseInt(e);
-      });
-    })
-    .map(function (e) {
-      a = a - e[0] + e[1];
-      return a;
-    })
-    .sort(function (a, b) {
-      return b - a;
-    })[0]
-);

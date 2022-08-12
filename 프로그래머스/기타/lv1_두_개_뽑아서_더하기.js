c0497d6912aad27c5ffd5cff547654c13213a3ed
josubin47 <3_3_jimca@naver.com>
2022-08-12 14:03:14 +0900

    Add 프로그래머스 / Lv1 / 두 개 뽑아서 더하기.js

---
+++ 프로그래머스/기타/lv1_두_개_뽑아서_더하기.js
@@ -0,0 +1,18 @@
+function solution(numbers) {
+  var answer = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      let sum = numbers[i] + numbers[j];
+      if (!answer.includes(sum)) {
+        answer.push(sum);
+      }
+    }
+  }
+
+  answer.sort(function (a, b) {
+    return a - b;
+  });
+
+  return answer;
+}

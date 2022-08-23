acebe6312b52074dcad0fda39d7a026ef71b069e
josubin47 <3_3_jimca@naver.com>
2022-08-23 15:00:22 +0900

    Add 프로그래머스 / Lv1 / 나누어 떨어지는 숫자 배열.js

---
+++ 프로그래머스/연습문제/lv1_나누어_떨어지는_숫자_배열.js
@@ -0,0 +1,17 @@
+function solution(arr, divisor) {
+  let answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor === 0) {
+      answer.push(arr[i]);
+    }
+  }
+
+  if (answer.length === 0) {
+    return [-1];
+  }
+
+  return answer.sort(function (a, b) {
+    return a - b;
+  });
+}
